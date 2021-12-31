0e08c930f8b8c0aee8e13c03e4c255717d3bf6cc
Carlos Duque <carlos.d.duque.r@gmail.com>
2021-12-31 17:18:45 -0500

    Adding responsive navbar transition effects

--- js/responsive-nav.js
+++ js/responsive-nav.js
@@ -1,9 +1,22 @@
 const hamburgerMenu = document.querySelector('.hamburger-menu');
 const navLinks = document.querySelector('.nav-links');
 const closeNav = document.querySelector('.close-nav');
+const navMobile = document.querySelector('.nav-mobile');
+let navIsActive = false;
 
 const toggleNav = () => {
-    navLinks.classList.toggle('hidden');
+
+    if (navIsActive) {
+        navMobile.classList.add('nav-mobile-transition');
+        navIsActive = false;
+    }
+    else {
+        navMobile.classList.remove('nav-mobile-transition');
+        navIsActive = true;
+    }
+
+    navMobile.classList.toggle('add-bg-color');
+    navLinks.classList.toggle('show-nav-links');
     hamburgerMenu.classList.toggle('hidden');
     closeNav.classList.toggle('hidden');
 };
